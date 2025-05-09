00c6e52328cfda66b9e3887893b4a8efcc3f5320
Sameera Agarwala <sameera.agarwala@gmail.com>
2025-05-08 19:04:59 -0700

    Changed formatting and added styles

---
+++ script.js
@@ -0,0 +1,13 @@
+const images = [
+    "assets/images/nationalConfBoard.JPG",
+    "assets/images/oSTEM Confrence 2019.jpg",
+  ];
+  
+  let current = 0;
+  const slide = document.getElementById("slide");
+  
+  setInterval(() => {
+    current = (current + 1) % images.length;
+    slide.src = images[current];
+  }, 3000); // change every 3 seconds
+  
