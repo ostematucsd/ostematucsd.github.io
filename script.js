d5a01c59ddec6dc21492bc1110b6541fdd9ed2c3
Sameera Agarwala <sameera.agarwala@gmail.com>
2025-05-08 19:27:54 -0700

    Added more CSS and design

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const images = [
-    "assets/images/nationalConfBoard.JPG",
-    "assets/images/oSTEM Confrence 2019.jpg",
+    "images/nationalConfBoard.JPG",
+    "images/oSTEM Confrence 2019.jpg",
   ];
   
   let current = 0;
@@ -10,4 +10,5 @@ const images = [
     current = (current + 1) % images.length;
     slide.src = images[current];
   }, 3000); // change every 3 seconds
+  
   
